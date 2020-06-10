6177633ea9518ff211175db4f9dc7ac096dcf266
Carlos Clayton <carlos.clayton@gmail.com>
2020-06-10 13:58:20 -0300

    Adding Authentication support

---
+++ resources/js/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+
+
+    export default {
+        name: 'app',
+        data () {
+            return {
+            }
+        },
+        created () {
+        },
+
+    }
+</script>
+
+<style>
+
+</style>
